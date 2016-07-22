a39b7bb0f5290f0ae388267de5ec33b35e6f4843
Xiang <zhangxiang1@jd.com>
2016-07-22 18:54:44 +0800

    user center

---
+++ LifeCalendar/LifeCalendar/VC/UserVC/UserCenterVC.h
@@ -0,0 +1,13 @@
+//
+//  UserCenterVC.h
+//  LifeCalendar
+//
+//  Created by Xiang on 16/7/22.
+//  Copyright © 2016年 iDreams. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface UserCenterVC : BaseViewController
+
+@end
---
+++ LifeCalendar/LifeCalendar/VC/UserVC/UserCenterVC.m
@@ -0,0 +1,13 @@
+//
+//  UserCenterVC.m
+//  LifeCalendar
+//
+//  Created by Xiang on 16/7/22.
+//  Copyright © 2016年 iDreams. All rights reserved.
+//
+
+#import "UserCenterVC.h"
+
+@implementation UserCenterVC
+
+@end
