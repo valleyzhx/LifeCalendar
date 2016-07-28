3ca5fed54c1668d52822488c94fa7d26573586ee
Xiang <zhangxiang1@jd.com>
2016-07-28 11:38:05 +0800

    userCenter 。。。

--- LifeCalendar/LifeCalendar/VC/UserVC/UserCenterVC.m
+++ LifeCalendar/LifeCalendar/VC/UserVC/UserCenterVC.m
@@ -8,6 +8,56 @@
 
 #import "UserCenterVC.h"
 
-@implementation UserCenterVC
+@implementation UserCenterVC{
+    NSArray *_titleArr;
+}
+
+-(void)viewDidLoad{
+    self.navType = GGNavigationBarTypeCustom;
+    self.showTable = YES;
+    [super viewDidLoad];
+    _titleArr = @[@[@"日历设置",@"灵感来源"],@[@"意见反馈",@"给个好评",@"分享APP"],@[@"每日一句"]];
+UILocalNotification
+}
+
+
+#pragma mark --- tableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return _titleArr.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSArray *arr = _titleArr[section];
+    return arr.count;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return section?10:0;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 10)];
+    view.backgroundColor = viewBGColor;
+    return view;
+}
+
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    cell.textLabel.textColor = TextDarkColor;
+    cell.textLabel.font = [UIFont systemFontOfSize:14];
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    if (indexPath.section!=1 ) {
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    cell.textLabel.text = _titleArr[indexPath.section][indexPath.row];
+    return cell;
+    
+    return nil;
+}
 
 @end
